a0c68052370ffa82e2405b70703ff46692277ca2
mustafagurturka-stack <mustafagurturka@gmail.com>
2026-08-21 12:55:22 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const CACHE_NAME = 'fittracker-pro-v277-4';
+const CACHE_NAME = 'fittracker-pro-v277-6';
 
 const APP_SHELL = [
   '/',
   '/index.html',
-  '/styles/main.css?v=177.4',
-  '/scripts/app.js?v=277.4',
-  '/manifest.json?v=277.4',
+  '/styles/main.css?v=177.6',
+  '/scripts/app.js?v=277.6',
+  '/manifest.json?v=277.6',
   '/icons/icon-192.png?v=229',
   '/icons/icon-512.png?v=229',
 ];
